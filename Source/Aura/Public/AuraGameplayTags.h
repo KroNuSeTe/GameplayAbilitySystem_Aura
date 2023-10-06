0efcc5791b9663fdbb3b5d0b4b7c75106a1422c3
Javier Dueñas <javiduecam@gmail.com>
2023-10-06 18:17:27 +0200

    Created a Singleton AuraGameplayTags with a struct of Primary and Secondary Tags, and Initialize from AuraAssetManager.

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,42 @@
+// Copyright KroNus Games 2023
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameplayTagContainer.h"
+
+/**
+ * AuraGameplayTags
+ *
+ * Singlenton containing native Gameplay Tags
+ * Singleton is a creational design pattern that lets you ensure that a class has only one instance, while providing a global access point to this instance.
+ */
+
+struct FAuraGameplayTags
+{
+public:
+    static const FAuraGameplayTags& Get(){return GameplayTags;}
+    static void InitializeNativeGameplayTags();
+
+	FGameplayTag Attributes_Secondary_Strength;
+	FGameplayTag Attributes_Secondary_Intelligence;
+	FGameplayTag Attributes_Secondary_Resilience;
+	FGameplayTag Attributes_Secondary_Vigor;
+
+    FGameplayTag Attributes_Secondary_Armor;
+	FGameplayTag Attributes_Secondary_ArmorPenetration;
+    FGameplayTag Attributes_Secondary_BlockChance;
+	FGameplayTag Attributes_Secondary_CriticalHitChance;
+	FGameplayTag Attributes_Secondary_CriticalHitDamage;
+	FGameplayTag Attributes_Secondary_CriticalHitResistance;
+	FGameplayTag Attributes_Secondary_HealthRegeneration;
+	FGameplayTag Attributes_Secondary_ManaRegeneration;
+	FGameplayTag Attributes_Secondary_MaxHealth;
+	FGameplayTag Attributes_Secondary_MaxMana;
+ 
+protected:
+
+private:
+ static FAuraGameplayTags GameplayTags;
+};
