df74c150b3fee23522923946d64a1be8cc0270a7
Javier Dueñas <javiduecam@gmail.com>
2023-10-06 19:23:21 +0200

    Created AttributeInfo with a structure to create in UE_IDE an Data Asset with Primary and Secondary Attributes, and a function to find the attribute to update. Added a AttributeMenuWidgetController to update this menu.

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,15 @@
+// Copyright KroNus Games 2023
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+	
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+	
+}
+
