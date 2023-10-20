60dd2e090f42367bc38ee8c2b5e1b9b490395b87
Javier Dueñas <javiduecam@gmail.com>
2023-10-20 11:34:55 +0200

    Add to DefaultGame.ini an script to change the AbilitySystemGlobalsClassName to AuraAbilitySystemGlobals, than we created and override the function AllocGameplayEffectContext to change for Aura one. Implement our own AuraAbilityTypes, NetSerialize override and add our 2 bools(bIsBlockHit & bIsCriticalHit.

---
+++ Source/Aura/Public/AbilitySystem/AuraAbilitySystemGlobals.h
@@ -0,0 +1,19 @@
+// Copyright KroNus Games 2023
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemGlobals.h"
+#include "AuraAbilitySystemGlobals.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraAbilitySystemGlobals : public UAbilitySystemGlobals
+{
+	GENERATED_BODY()
+
+	virtual FGameplayEffectContext* AllocGameplayEffectContext() const override;
+	
+};
--- Source/Aura/Public/AuraAbilityTypes.h
+++ Source/Aura/Public/AuraAbilityTypes.h
@@ -11,7 +11,7 @@ struct FAuraGameplayEffectContext : public FGameplayEffectContext
 
 public:
 	bool IsCriticalHit() const {return bIsCriticalHit;}
-	bool IsBockedHit() const {return bIsBlockHit;}
+	bool IsBlockedHit() const {return bIsBlockHit;}
 
 	void SetIsCriticalHit(bool bInIsCriticalHit) {bIsCriticalHit = bInIsCriticalHit;}
 	void SetBlockedHit(bool bInIsBlockedHit) {bIsBlockHit = bInIsBlockedHit;}
@@ -19,11 +19,24 @@ public:
 	/** Returns the actual struct used for serialization, subclasses must override this! */
 	virtual UScriptStruct* GetScriptStruct() const
 	{
-		return FGameplayEffectContext::StaticStruct();
+		return StaticStruct();
 	}
 
 	/** Custom serialization, subclasses must override this */
 	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+
+	/** Creates a copy of this context, used to duplicate for later modifications */
+	virtual FGameplayEffectContext* Duplicate() const
+	{
+		FAuraGameplayEffectContext* NewContext = new FAuraGameplayEffectContext();
+		*NewContext = *this;
+		if (GetHitResult())
+		{
+			// Does a deep copy of the hit result
+			NewContext->AddHitResult(*GetHitResult(), true);
+		}
+		return NewContext;
+	}
 	
 protected:
 	UPROPERTY()
@@ -32,4 +45,14 @@ protected:
 	UPROPERTY()
 	bool bIsCriticalHit = false;
 	
+};
+
+template<>
+struct TStructOpsTypeTraits<FAuraGameplayEffectContext> : public TStructOpsTypeTraitsBase2<FAuraGameplayEffectContext>
+{
+	enum
+	{
+		WithNetSerializer = true,
+		WithCopy = true
+	};
 };
